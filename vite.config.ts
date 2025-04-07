9ebd9afd5e70a97c64e14e287bb8691b62b2aa83
shamiyy <usamahsnnn@gmail.com>
2025-04-07 15:31:14 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-<<<<<<< HEAD
-=======
 
->>>>>>> 72c640cca21cc5907e54acc79aad63d38cf47efe
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
